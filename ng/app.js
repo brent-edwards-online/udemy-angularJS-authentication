491549409dce21da0b6bc3653adb15877234d34a
brent-edwards-online <brent@brentedwardsonline.com>
2017-07-11 14:23:04 +1000

    no message

--- ng/app.js
+++ ng/app.js
@@ -3,7 +3,12 @@
 angular.module('authApp',['auth0','angular-storage','angular-jwt','ngMaterial','ui.router'])
 .config(function($provide, authProvider, $urlRouterProvider, $stateProvider, $httpProvider, jwtInterceptorProvider){
 
-  
+  var auth= $injector.get('auth');
+
+  authProvider.init({
+    domain:'beonline.au.auth0.com',
+    clientID:'3vLtTr1yEkre74rq5u5TCtVlCMjagQuo'
+  });
   
   $urlRouterProvider.otherwise('/home');
 
@@ -17,4 +22,40 @@ angular.module('authApp',['auth0','angular-storage','angular-jwt','ngMaterial','
       templateUrl: 'components/profile/profile.tpl.html',
       controller: 'profileController as user'
     });
+
+  function redirect($q, $injector, auth, store, $location){
+    return {
+      responseError: function(rejection){
+        if(rejection.status === 401){
+          auth.signout();
+          store.remove(profile);
+          store.remove(token);
+          $location.path('/home');
+        }
+
+        return $q.reject(rejection);
+      }
+    }
+  }
+
+  $provide.factory('redirect', redirect);
+  $httpProvider.interceptors.push('redirect');
+  //$httpProvider.interceptors.push('jwtInterceptor');
+
+
+})
+.run(function($rootScope, auth, store, jwtHelper, $location){
+  $rootScope.$on('$locationChangeStart', function(){
+    var token = store.get('token');
+    if(token) {
+      if(!jwtHelper.isTokenExpired(token)){
+        if(!auth.isAuthenticated){
+          auth.authenticate(store.get('profile'), token);
+        }
+      }
+    }
+    else{
+      $location.path('/home');
+    }
+  })
 });
